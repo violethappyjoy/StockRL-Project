f3c919d1f16b5f8cb859763cad0c74dab3971bb8
violethappyjoy <21bce026@nirmauni.ac.in>
2023-08-08 21:36:06 +0530

    6.2 COMMIT!!

--- graphs.py
+++ graphs.py
@@ -11,6 +11,7 @@ avg_reward = pd.read_csv('graphs/reward_avg.csv')
 x = accuracy.Step.to_numpy()
 y = accuracy.Value.to_numpy()
 
+plt.figure(figsize=(8, 8))
 plt.plot(x, y, c='b')
 params = {'axes.linewidth': 2, 'xtick.major.width': 2, 'ytick.major.width': 2, 'font.size': 14}
 plt.rcParams.update(params)
@@ -23,14 +24,14 @@ plt.show()
 
 x = loss.Step.to_numpy()
 y = loss.Value.to_numpy()
-print(y)
-
+# print(y)
+plt.figure(figsize=(8.5, 8.5))
 plt.plot(x, y, c='b')
 params = {'axes.linewidth': 2, 'xtick.major.width': 2, 'ytick.major.width': 2, 'font.size': 14}
 plt.rcParams.update(params)
 plt.xlabel('Steps')
 plt.ylabel('Value')
-
+plt.subplots_adjust( wspace=1.0, hspace=1.0)
 y_min = min(y)
 y_max = max(y)
 y_range = y_max - y_min
@@ -43,11 +44,13 @@ plt.show()
 x = avg_reward.Step.to_numpy()
 y = avg_reward.Value.to_numpy()
 
+plt.figure(figsize=(8.1, 8.1))
 plt.plot(x, y, c='b')
 params = {'axes.linewidth': 2, 'xtick.major.width': 2, 'ytick.major.width': 2, 'font.size': 14}
 plt.rcParams.update(params)
 plt.xlabel('Steps')
 plt.ylabel('Value')
+# plt.subplots_adjust(left=0.15, right=0.99, top=0.95, bottom=0.15)
 
 plt.savefig('graphs/Avg_Reward.png', dpi = 400)
 plt.savefig('graphs/Avg_Reward.pdf', dpi = 400)
